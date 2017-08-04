8a7bb5b6f8f7ec13c1e109f959448cc6bbf64bd3
Igor Makhtes <igor@cogniteam.com>
2017-08-04 19:17:35 +0300

    talker version 2

--- basic_ros_pkg/src/talker.cpp
+++ basic_ros_pkg/src/talker.cpp
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
     std_msgs::String msg;
 
     std::stringstream ss;
-    ss << "hello world " << count;
+    ss << "hello world 2" << count;
     msg.data = ss.str();
 
     ROS_INFO("%s", msg.data.c_str());
